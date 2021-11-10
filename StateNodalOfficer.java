b3556ce1bc4bda120e69e7a5bff5614752a0a976
Sheba-John <92512577+Sheba-John@users.noreply.github.com>
2021-11-10 12:30:47 +0530

    Add files via upload

---
+++ StateNodalOfficer.java
@@ -0,0 +1,40 @@
+package com.example.nsplayer2;
+
+public class StateNodalOfficer {
+private String StateName;
+private String Password;
+private String OfficerName;
+private int MobileNumber;
+private String EmailID;
+public String getStateName() {
+	return StateName;
+}
+public void setStateName(String stateName) {
+	StateName = stateName;
+}
+public String getPassword() {
+	return Password;
+}
+public void setPassword(String password) {
+	Password = password;
+}
+public String getOfficerName() {
+	return OfficerName;
+}
+public void setOfficerName(String officerName) {
+	OfficerName = officerName;
+}
+public int getMobileNumber() {
+	return MobileNumber;
+}
+public void setMobileNumber(int mobileNumber) {
+	MobileNumber = mobileNumber;
+}
+public String getEmailID() {
+	return EmailID;
+}
+public void setEmailID(String emailID) {
+	EmailID = emailID;
+}
+
+}
