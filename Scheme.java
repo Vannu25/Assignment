232b037b2ffecab81b8153af0ecad74505b7be9d
Sheba-John <92512577+Sheba-John@users.noreply.github.com>
2021-11-10 12:29:53 +0530

    Add files via upload

---
+++ Scheme.java
@@ -0,0 +1,40 @@
+package com.example.nsplayer2;
+
+import java.time.LocalDate;
+
+public class Scheme {
+private int SchemeID;	
+private String SchemeName;
+private LocalDate LastApplyDate;
+private int Amount;
+public int getSchemeID() {
+	return SchemeID;
+}
+public void setSchemeID(int schemeID) {
+	SchemeID = schemeID;
+}
+public String getSchemeName() {
+	return SchemeName;
+}
+public void setSchemeName(String schemeName) {
+	SchemeName = schemeName;
+}
+public LocalDate getLastApplyDate() {
+	return LastApplyDate;
+}
+public void setLastApplyDate(LocalDate lastApplyDate) {
+	LastApplyDate = lastApplyDate;
+}
+public int getAmount() {
+	return Amount;
+}
+public void setAmount(int amount) {
+	Amount = amount;
+}
+
+}
+	
+	
+	
+	
+
